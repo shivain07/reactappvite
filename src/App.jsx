2a9b93b72cc41f76e96b6e28b5b1ebb8a0721428
shivain07 <shivaingusain007@gmail.com>
2025-04-13 02:56:26 +0530

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <h2>Hey this is working champ</h2>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
